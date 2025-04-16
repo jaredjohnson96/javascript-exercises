376e0372b86386be5452d7752a6b1851b1506f77
jaredjohnson96 <jaredj19@gmail.com>
2025-04-16 09:49:56 -0700

    Exercise 3 commit

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function(astring) {    
+    const len = astring.length
+    let revastring = ""
+    for (let i = 0; i < len; i++) {
+        revastring = astring[i] + revastring
+    }
+    return revastring
+    
 
 };
+
+reverseString ("hello")
 
 // Do not edit below this line
 module.exports = reverseString;
