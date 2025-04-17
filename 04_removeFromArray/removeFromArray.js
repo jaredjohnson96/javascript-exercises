8117949435af7abd587fa7aa694969243b64466a
jaredjohnson96 <jaredj19@gmail.com>
2025-04-16 20:21:22 -0700

    Latest exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array1, ...input1) {
+    input1 = [...input1]
+    const len = array1.length
+    console.log(len)
+    for (var i of input1){
+        while (array1.indexOf(i)>=0){
+                array1.splice(array1.indexOf(i),1)
+                console.log(array1)
+            }
+        }
+        console.log(array1)
+        return array1
+    } ;
+
+removeFromArray([1,2,4,4,2,2],2,4)
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,30 @@
-const sumAll = function() {
-
+const sumAll = function(n1, n2) {
+    if(n1>0 && n2>0) {
+        if (typeof(n1) === 'number' && typeof(n2) === 'number') {
+            if (Number.isInteger(n1) && Number.isInteger(n2)){
+                m1 = Math.min(n1,n2)
+                m2 = Math.max(n1,n2)
+                let sum = 0
+                    for (i = m1; i <= m2; i++){
+                        sum += i
+                    }
+                    console.log(sum)
+                    return sum
+                }            
+            else {
+                return 'ERROR'
+            }
+        }
+        else {
+            return 'ERROR'
+        }
+    }      
+    else {
+        return 'ERROR';
+    }
 };
+
+sumAll(1.1,1)
 
 // Do not edit below this line
 module.exports = sumAll;
